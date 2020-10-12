78be8a889ed8eb07ebbb29eaad2f0aaf2194098f
Glitch (stripe-impossible-duke) <none>
2020-10-12 11:42:54 +0000

    Made By Hemant

---
+++ script.js
@@ -0,0 +1,86 @@
+window.episodes = [];
+
+var headings = [
+  "Remember this one?",
+  "Oh, this one is great!",
+  "I love this one!"
+];
+
+var getEpisodes = function() {
+  $.getJSON('/episodes.json', function(episodes) {
+    console.info('Episodes loaded!', episodes);
+    window.episodes = episodes;
+  })
+    .done(function() {
+      init();
+    })
+    .fail(function() {
+      $('.card').replaceWith('<button class="error" onClick="window.location.reload()">Error loading episodes, click here to reload.</button>');
+    });
+};
+
+var randomItem = function(array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+var getRandomEpisode = function() {
+  var season = randomItem(window.episodes),
+      episode = randomItem(season.episodes);
+
+  // add logic to make sure the first episode in 2-part episodes always comes first?
+
+  console.log('random episode: ', season.season, episode);
+
+  // Update the URLs
+  $('.card a')
+    .attr('href', 'https://www.netflix.com' + episode.urls.netflix)
+    .eq(0).text('S' + season.season + ' E' + episode.episode + ' - ' + episode.title);
+  
+  // Update the header
+  $('h1').text(randomItem(headings));
+  
+  // Update the elements for data tracking
+  $('a, button', '.card').data({
+    episode: episode.episode + ' - ' + episode.title,
+    season: season.season
+  });
+  
+  // TODO: Add episode descriptions to JSON and load them into card
+};
+
+// event tracking
+var trackCardClick = function($target) {
+  var action = $target.data('gaAction');
+  var label  = $target.data('episode');
+  var value  = $target.data('season');
+  var event  = {
+    eventCategory: 'Card Click',
+    eventAction: action, // button or link action
+    eventLabel: label, // episode number and name
+    eventValue: value // season
+  };
+
+  window.ga('send', 'event', event);
+};
+
+// I don't love this name. What's better, or is there a better way to do this altogether?
+var init = function() {
+  getRandomEpisode();
+  
+  // Show the card if hidden
+  $('.card.hidden').removeClass('hidden');
+  
+  // Bind UI handlers
+  $('a, button', '.card').on('click', function() {
+    trackCardClick($(this));
+  });
+  
+  $('.card button').on('click', function() {
+    getRandomEpisode();
+  });
+};
+
+// All set, let's go!
+$(function() {
+  getEpisodes();
+});
